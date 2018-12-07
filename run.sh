7d02510b70ec4871e2be06f6581a4d65a2a69aad
Winchester <gabrielws31@gmail.com>
2018-12-06 23:55:47 -0200

    Resultados Gabriel e Thiago (já arrumei os ponto por virgula)

--- run.sh
+++ run.sh
@@ -1,16 +1,32 @@
 # Quantidade de repeticoes desejada
 NUM_REPETICOES=50
 RANDOM=12833
-PORCENTAGEM=80
-INSTANCIA=graph250-01
-# Reseta o contador inicial
-CONTADOR=0
-# Executa a aplicacao a quantidade de repeticoes especificada
-until [ $CONTADOR -ge $NUM_REPETICOES ];
+PORCENTAGEM=60
+INSTANCIA=("hamming-10-4" "vc4")
+TAMANHO=${#INSTANCIA[@]}
+for ((i=0;i<$TAMANHO;i++))
 do
-echo "Execucao No. $CONTADOR"
-echo
-python main.py $RANDOM $PORCENTAGEM g "$INSTANCIA.txt" "resultado-$INSTANCIA" $CONTADOR
-# Incrementa o contador
-let CONTADOR=CONTADOR+1
+    CONTADOR=0
+    # Executa a aplicacao a quantidade de repeticoes especificada
+    until [ $CONTADOR -ge $NUM_REPETICOES ];
+    do
+    echo "Execucao No. $CONTADOR do arquivo ${INSTANCIA[$i]}"
+    echo
+    python main.py $RANDOM $PORCENTAGEM g "${INSTANCIA[$i]}.txt" "resultado-${INSTANCIA[$i]}"
+    # Incrementa o contador
+    let CONTADOR=CONTADOR+1
+    done
+done
+for ((i=0;i<$TAMANHO;i++))
+do
+    CONTADOR=0
+    # Executa a aplicacao a quantidade de repeticoes especificada
+    until [ $CONTADOR -ge $NUM_REPETICOES ];
+    do
+    echo "Execucao No. $CONTADOR do arquivo ${INSTANCIA[$i]}"
+    echo
+    python main.py $RANDOM $PORCENTAGEM a "${INSTANCIA[$i]}.txt" "resultado-${INSTANCIA[$i]}"
+    # Incrementa o contador
+    let CONTADOR=CONTADOR+1
+    done
 done
